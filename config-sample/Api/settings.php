4557ce7f6e38bb33c4dc8699cdaef17c914484b4
javinc <javinczki02@gmail.com>
2015-09-01 17:01:06 +0800

    update config sample

--- config-sample/Api/settings.php
+++ config-sample/Api/settings.php
@@ -1,11 +1,21 @@
 <?php //-->
 
+$host = 'http://api.ejr.dev';
+
 return array(
-	'url_root'			=> 'http://api.sample.dev',
-	'cdn_root'			=> '',
-	'i18n'				=> 'en_US',
-	'eden_debug' 		=> true,
-	'debug_mode' 		=> E_ALL,
-    'uac'               => false,
-	'server_timezone' 	=> 'Asia/Manila',
-	'default_page' 		=> 'index');
+	'url_root' => $host,
+	'cdn_root' => '',
+	'i18n' => 'en_US',
+	'eden_debug' => true,
+    'uac' => true,
+	'debug_mode' => E_ALL,
+	'server_timezone' => 'Asia/Manila',
+	'default_page' => 'index',
+	'jwt' => array(
+		'key' => 'whatthefucklogic',
+		'algo' => array('HS256'),
+		'payload' => array(
+			'iss' => $host,
+            'aud' => 'http://ejr.dev',
+            'iat' => 1356999524,
+            'nbf' => 1357000000)));
