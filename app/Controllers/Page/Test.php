4e38ee8087d00341dea2194678d59dfbdf47cfdd
javinc <javinczki02@gmail.com>
2015-11-20 17:44:04 +0800

    change file structure

---
+++ app/Controllers/Page/Test.php
@@ -0,0 +1,29 @@
+<?php //-->
+
+namespace Api\Page;
+
+use Modules\Rest;
+use Services\Test as T;
+
+class Test extends \Page
+{
+    /* Constants
+    --------------------------------------------*/
+    /* Public Properties
+    --------------------------------------------*/
+    // public $auth = false;
+
+    /* Protected Properties
+    --------------------------------------------*/
+    /* Public Methods
+    --------------------------------------------*/
+    public function getVariables()
+    {
+        return Rest::resource(new T(), true);
+    }
+
+    /* Protected Methods
+    --------------------------------------------*/
+    /* Private Methods
+    --------------------------------------------*/
+}
