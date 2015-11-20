d5673a9be52e68d396f2b75df4e074fa977a6c75
javinc <javinczki02@gmail.com>
2015-11-20 17:43:37 +0800

    change file structure

---
+++ app/App.php
@@ -0,0 +1,33 @@
+<?php //-->
+
+namespace App;
+
+use Exception;
+use Modules\Helper;
+use Modules\Upload;
+use Resources\File as F;
+
+/**
+ * Service File
+ * business logic of this class object
+ *
+ * @category   service
+ * @author     javincX
+ */
+class App
+{
+    /* Constants
+    --------------------------------------------*/
+    /* Public Properties
+    --------------------------------------------*/
+    /* Protected Properties
+    --------------------------------------------*/
+    /* Private Properties
+    --------------------------------------------*/
+    /* Public Methods
+    --------------------------------------------*/
+    /* Protected Methods
+    --------------------------------------------*/
+    /* Private Methods
+    --------------------------------------------*/
+}
