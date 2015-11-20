58ebc59704b9b89b5b9473a45f403d1ecbb6be81
javinc <javinczki02@gmail.com>
2015-11-20 16:56:31 +0800

    service layer added

---
+++ repo/Services/User.php
@@ -0,0 +1,111 @@
+<?php //-->
+
+namespace Services;
+
+use Modules\Auth;
+use Modules\Helper;
+use Resources\User as U;
+use Resources\UserSetting;
+
+/**
+ * Service User
+ * business logic of this class object
+ *
+ * @category   service
+ * @author     javincX
+ */
+class User
+{
+    /* Constants
+    --------------------------------------------*/
+    const ADMIN_TYPE = 1;
+    const CLIENT_TYPE = 0;
+
+    const USER_FIELD = 'email';
+    const PASS_FIELD = 'password';
+    const STATUS_FIELD = 'status';
+
+    /* Public Properties
+    --------------------------------------------*/
+    public static $required = array(
+        'create' => array(
+            'first_name',
+            'last_name',
+            'email',
+            'password'));
+
+    /* Protected Properties
+    --------------------------------------------*/
+    /* Private Properties
+    --------------------------------------------*/
+    /* Public Methods
+    --------------------------------------------*/
+    public static function __callStatic($name, $args)
+    {
+        return U::$name(current($args), end($args));
+    }
+
+    public static function create($data)
+    {
+        // check if email exists
+        if(U::get(array(
+            'filters' => array(
+                'email' => $data['email']),
+            'fields' => array('id')))) {
+            return Helper::error('USER_EMAIL_EXISTS',
+                '\'' . $data['email'] . '\' email already exists');
+        }
+
+        // hash id
+        $data['password'] = sha1($data['password']);
+
+        // save the user
+        $user = U::create($data);
+
+        return $user;
+    }
+
+    public static function update($data, $filter)
+    {
+        // check password if exists then update it
+        // else dont
+        if(isset($data['password']) && trim($data['password']) != '') {
+            $data['password'] = sha1($data['password']);
+        } else if(trim($data['password']) == '') {
+            unset($data['password']);
+        }
+
+        // save the user
+        $user = U::update($data, $filter);
+
+        return $user;
+    }
+
+    public static function getClientById($id, $options = array())
+    {
+        $options['filters']['id'] = $id;
+        $options['filters']['role_id'] = self::CLIENT_TYPE;
+
+        return self::get($options);
+    }
+
+    public static function findByType($type, $options = array())
+    {
+        // default will be client
+        $roleId = self::CLIENT_TYPE;
+        switch ($type) {
+        case 'admin':
+            $roleId = self::ADMIN_TYPE;
+            break;
+        }
+
+        $options['filters']['role_id'] = $roleId;
+
+        return self::find($options);
+    }
+
+    /* Protected Methods
+    --------------------------------------------*/
+    /* Private Methods
+    --------------------------------------------*/
+}
